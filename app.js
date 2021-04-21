ce8f31ecf056531b65fab7728ebdfcaaba848648
Bill Hong <billhong1510@gmail.com>
2021-04-21 14:28:29 -0700

    commit 4

--- app.js
+++ app.js
@@ -21,3 +21,10 @@ x.setAttribute("src", "Album4.png");
 x.setAttribute("width", "200");
 x.setAttribute("height", "200");
 document.body.appendChild(x)
+
+const albums = [
+    { name: "Album 1", price: 12.99, inCart: false }, 
+    { name: "Album 2", price: 14.99, inCart: false }, 
+    { name: "Album 3", price: 9.99, inCart: false }, 
+    { name: "Album 4", price: 19.99, inCart: false }
+  ];
