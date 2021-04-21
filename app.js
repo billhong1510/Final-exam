d42c452a238a6390bdc23f8ba15ae0e11b897080
Bill Hong <billhong1510@gmail.com>
2021-04-21 14:28:07 -0700

    commit 3

--- app.js
+++ app.js
@@ -3,3 +3,21 @@ x.setAttribute("src", "Album1.png");
 x.setAttribute("width", "200");
 x.setAttribute("height", "200");
 document.body.appendChild(x)
+
+var img = document.createElement("img");
+x.setAttribute("src", "Album2.png");
+x.setAttribute("width", "200");
+x.setAttribute("height", "200");
+document.body.appendChild(x)
+
+var img = document.createElement("img");
+x.setAttribute("src", "Album3.png");
+x.setAttribute("width", "200");
+x.setAttribute("height", "200");
+document.body.appendChild(x)
+
+var img = document.createElement("img");
+x.setAttribute("src", "Album4.png");
+x.setAttribute("width", "200");
+x.setAttribute("height", "200");
+document.body.appendChild(x)
